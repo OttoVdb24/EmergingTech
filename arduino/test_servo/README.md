da918481382257fc6c054d4c713664a7bb837037
OttoVdb24 <otto@telenet.be>
2026-04-14 16:22:44 +0200

    Servo functie updaten zodat deze meerdere servo's kan aansturen in verschillende hoeken.

--- arduino/test_servo/README.md
+++ arduino/test_servo/README.md
@@ -2,6 +2,11 @@
 
 Deze test heeft als doel een functie schrijven die op commanodo de arm van uitgestrekte naar ingeplooide toestand kan verplaatsen volgens een gecontroleerd pad. Er wordt gewerkt met een duuwknop om de trigger te initiëren.
 
+**V2:**
+De tweede versie werd de code aangepast om dezelfde functie te kunnen gebruiken voor twee of meerdere servo's. Dit werd gedaan door het servo en ramp object te importeren in de functie. 
+Ook de in en uit posititie werd als input van de functie geschreven om fouten in de servo aansturing te corrigeren.
+Extra werd de start postitie geschreven m.b.v. de Ramp functie's.
+
 **In deze code werden volgende librarys gebruikt:**
 - Servo library[^1]
 - Ramp library[^2]
--- arduino/test_servo/test_servo.ino
+++ arduino/test_servo/test_servo.ino
@@ -1,58 +1,59 @@
 #include <Ramp.h>
 #include <Servo.h>
 
-rampInt servoRamp;   // Servo ramp object aanmaken
-Servo myservo;  //Aanmaken van het servo object
+rampInt servoRamp_1;   // Servo ramp object aanmaken
+rampInt servoRamp_2;
+Servo Servo_1;  //Aanmaken van het servo object
+Servo Servo_2;
 
-int inPos = 0;       // de positie wanneer de arm ingeklapd is.
-int uitPos = 120;   // de positite wanneer de arm uitgestrekt is
-int servoPos;        // globale variabele, de effectieve hoek van de servo.
 
-bool servoRampActive = false;
+bool servoRampActive_1 = false;
+bool servoRampActive_2 = false;
 
 bool flankDetectieBytes[2];
 
-void servoRampFunctie(){
-
-  if (!servoRampActive && servoPos==inPos){
-  servoRampActive=true;
-  servoRamp.go(uitPos,1000,QUINTIC_OUT);
-  }
-  else if (!servoRampActive && servoPos==uitPos){
-  servoRampActive=true;
-  servoRamp.go(inPos,1000,QUINTIC_IN);
-  }
 
 
-  if (servoRampActive){
-  servoPos = servoRamp.update();
-  myservo.write(servoPos);  
-  }
 
-  if (servoRamp.isFinished()){
-    servoRampActive=false;
-  }
-}
+
+
+
+
+
+
+
 
 
 
 void setup() {
 
   Serial.begin(9600);
-  myservo.attach(9);  // attaches the servo on pin 9 to the Servo object
-  myservo.write(inPos);
-  servoRamp.setGrain(1);
+  Servo_1.attach(9);  // attaches the servo on pin 9 to the Servo object
+  Servo_2.attach(10);  // attaches the servo on pin 9 to the Servo object
 
+
+  servoRamp_1.setGrain(1);
+  servoRamp_2.setGrain(1);
+
+  servoRamp_1.go(0, 0, LINEAR);   // initialiseer op startpositie
+  servoRamp_2.go(90, 0, LINEAR);  // initialiseer op startpositie
+  Servo_1.write(servoRamp_1.update());
+  Servo_2.write(servoRamp_2.update());
+
+  pinMode(13,OUTPUT);
 }
 
 
 void loop() {
 
   bool startKnop = positieveFlankDetectie(digitalRead(2),flankDetectieBytes[0]);
-  if (startKnop || servoRampActive){
-    servoRampFunctie();
+  if (startKnop || servoRampActive_1){
+    servoRampActive_1 = servoRampFunctie(Servo_1,servoRamp_1, servoRampActive_1, 0, 90); 
   }
-
+  if (startKnop || servoRampActive_2){
+    servoRampActive_2 = servoRampFunctie(Servo_2,servoRamp_2, servoRampActive_2, 90,0); 
+    Serial.println("2,Actief");
+  }
  }
 
 
@@ -74,4 +75,29 @@ bool positieveFlankDetectie(bool Input, bool &flankDetectieByte){
     flankDetectieByte=false;
     return false;
   }
+}
+
+bool servoRampFunctie(Servo &myservo, rampInt &servoRamp, bool servoRampActive, int inPos, int uitPos) {
+
+  if (servoRamp.isFinished() && servoRampActive) {
+    servoRampActive = false;
+    digitalWrite(13, LOW);
+    return false;  // Functie doen stoppen.
+  }
+
+  if (!servoRamp.isRunning() && servoRamp.getValue() == inPos) { // Start van de actie motorbeweging , checkt of die uit of in moet bewegen
+    servoRampActive = true;
+    servoRamp.go(uitPos, 3000, QUADRATIC_IN);
+  }
+  else if (!servoRamp.isRunning() && servoRamp.getValue() == uitPos) {
+    servoRampActive = true;
+    servoRamp.go(inPos, 3000, QUADRATIC_IN );
+  }
+
+  if (servoRamp.isRunning()) {      // De postitie van de servi veranderen wanneer de Ramp actief is. + controle led doen branden.
+    myservo.write(servoRamp.update());
+    digitalWrite(13, HIGH);
+    return true;
+  }
+
 }
